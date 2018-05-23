96bb0160a67ca3b39d3b94b6f81bccf1949ac3ac
Branden Dane <danetheory@gmail.com>
2018-05-23 10:23:19 -0400

    port change 2

--- tools/prodServer.js
+++ tools/prodServer.js
@@ -37,4 +37,4 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
